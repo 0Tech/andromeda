8640da69cff4d2302b9d43dbcf3a7e85bc6c1d85
Youngtaek Yoon <noreply@yoon.mailer.me>
2023-10-31 14:26:00 +0000

    Update module.go

--- x/internft/module/module.go
+++ x/internft/module/module.go
@@ -5,13 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 
-	abci "github.com/cometbft/cometbft/abci/types"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/spf13/cobra"
 
 	"cosmossdk.io/core/appmodule"
+	"cosmossdk.io/core/store"
 	"cosmossdk.io/depinject"
-	storetypes "cosmossdk.io/store/types"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -21,46 +20,52 @@ import (
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 
-	internft "github.com/0tech/andromeda/x/internft/andromeda/internft/v1alpha1"
+	internftv1alpha1 "github.com/0tech/andromeda/x/internft/andromeda/internft/v1alpha1"
 	modulev1alpha1 "github.com/0tech/andromeda/x/internft/api/andromeda/internft/module/v1alpha1"
 	"github.com/0tech/andromeda/x/internft/client/cli"
 	"github.com/0tech/andromeda/x/internft/keeper"
 )
 
+// ----------------------------------------------------------------------------
+// AppModuleBasic
+// ----------------------------------------------------------------------------
+
 // AppModuleBasic defines the basic application module used by the module.
 type AppModuleBasic struct{}
 
-var _ module.AppModuleBasic = (*AppModuleBasic)(nil)
-
-// Name returns the ModuleName
-func (AppModuleBasic) Name() string {
-	return internft.ModuleName
+// GetQueryCmd returns the cli query commands for the module.
+func (AppModuleBasic) GetQueryCmd() *cobra.Command {
+	return cli.NewQueryCmd()
 }
 
-// RegisterLegacyAminoCodec registers the types on the LegacyAmino codec
-func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
-	internft.RegisterLegacyAminoCodec(cdc)
-}
-
-func (b AppModuleBasic) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
-	internft.RegisterInterfaces(registry)
-}
-
-// RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
-func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
-	if err := internft.RegisterQueryHandlerClient(context.Background(), mux, internft.NewQueryClient(clientCtx)); err != nil {
-		panic(err)
-	}
-}
-
-// GetTxCmd returns the transaction commands for the module
+// GetTxCmd returns the cli transaction commands for the module.
 func (AppModuleBasic) GetTxCmd() *cobra.Command {
 	return cli.NewTxCmd()
 }
 
-// GetQueryCmd returns the cli query commands for the module
-func (AppModuleBasic) GetQueryCmd() *cobra.Command {
-	return cli.NewQueryCmd()
+// ____________________________________________________________________________
+
+var _ module.AppModuleBasic = (*AppModuleBasic)(nil)
+
+// Name returns the name of the module.
+func (AppModuleBasic) Name() string {
+	return internftv1alpha1.ModuleName
+}
+
+// RegisterLegacyAminoCodec registers the types on the LegacyAmino codec
+func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	internftv1alpha1.RegisterLegacyAminoCodec(cdc)
+}
+
+func (AppModuleBasic) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
+	internftv1alpha1.RegisterInterfaces(registry)
+}
+
+// RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
+func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
+	if err := internftv1alpha1.RegisterQueryHandlerClient(context.Background(), mux, internftv1alpha1.NewQueryClient(clientCtx)); err != nil {
+		panic(err)
+	}
 }
 
 // ____________________________________________________________________________
@@ -69,20 +74,22 @@ var _ module.HasGenesisBasics = (*AppModuleBasic)(nil)
 
 // DefaultGenesis returns default genesis state as raw bytes for the module.
 func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
-	return cdc.MustMarshalJSON(internft.DefaultGenesisState())
+	return cdc.MustMarshalJSON(internftv1alpha1.DefaultGenesisState())
 }
 
 // ValidateGenesis performs genesis state validation for the module.
 func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config client.TxEncodingConfig, bz json.RawMessage) error {
-	var gs internft.GenesisState
+	var gs internftv1alpha1.GenesisState
 	if err := cdc.UnmarshalJSON(bz, &gs); err != nil {
-		return fmt.Errorf("failed to unmarshal %s genesis state: %w", internft.ModuleName, err)
+		return fmt.Errorf("failed to unmarshal %s genesis state: %w", internftv1alpha1.ModuleName, err)
 	}
 
 	return gs.ValidateBasic()
 }
 
-// ____________________________________________________________________________
+// ----------------------------------------------------------------------------
+// AppModule
+// ----------------------------------------------------------------------------
 
 var _ module.AppModule = (*AppModule)(nil)
 
@@ -93,7 +100,7 @@ type AppModule struct {
 	keeper keeper.Keeper
 }
 
-// NewAppModule creates a new AppModule object
+// NewAppModule creates a new AppModule object.
 func NewAppModule(cdc codec.Codec, keeper keeper.Keeper) AppModule {
 	return AppModule{
 		keeper: keeper,
@@ -104,24 +111,23 @@ func NewAppModule(cdc codec.Codec, keeper keeper.Keeper) AppModule {
 
 var _ module.HasInvariants = (*AppModule)(nil)
 
-// RegisterInvariants does nothing, there are no invariants to enforce
-func (am AppModule) RegisterInvariants(ir sdk.InvariantRegistry) {}
+func (am AppModule) RegisterInvariants(ir sdk.InvariantRegistry) {
+	// TODO(@0Tech): add invariants
+}
 
 // ____________________________________________________________________________
 
 var _ module.HasServices = (*AppModule)(nil)
 
-// RegisterServices registers a GRPC query service to respond to the
-// module-specific GRPC queries.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
-	internft.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServer(am.keeper))
-	internft.RegisterQueryServer(cfg.QueryServer(), keeper.NewQueryServer(am.keeper))
+	internftv1alpha1.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServer(am.keeper))
+	internftv1alpha1.RegisterQueryServer(cfg.QueryServer(), keeper.NewQueryServer(am.keeper))
 
 	// m := keeper.NewMigrator(am.keeper)
 	// migrations := map[uint64]func(sdk.Context) error{}
 	// for ver, handler := range migrations {
-	// 	if err := cfg.RegisterMigration(internft.ModuleName, ver, handler); err != nil {
-	// 		panic(fmt.Sprintf("failed to migrate x/%s from version %d to %d: %v", internft.ModuleName, ver, ver+1, err))
+	// 	if err := cfg.RegisterMigration(internftv1alpha1.ModuleName, ver, handler); err != nil {
+	// 		panic(fmt.Sprintf("failed to migrate x/%s from version %d to %d: %v", internftv1alpha1.ModuleName, ver, ver+1, err))
 	// 	}
 	// }
 }
@@ -130,22 +136,19 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 
 var _ module.HasGenesis = (*AppModule)(nil)
 
-// InitGenesis performs genesis initialization for the module. It returns
-// no validator updates.
-func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.RawMessage) []abci.ValidatorUpdate {
-	var gs internft.GenesisState
+// InitGenesis performs genesis initialization for the module.
+func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.RawMessage) {
+	var gs internftv1alpha1.GenesisState
 	cdc.MustUnmarshalJSON(data, &gs)
 
-	if err := am.keeper.InitGenesis(ctx, &gs); err != nil {
+	if err := am.keeper.InitGenesis(sdk.UnwrapSDKContext(ctx), &gs); err != nil {
 		panic(err)
 	}
-
-	return []abci.ValidatorUpdate{}
 }
 
 // ExportGenesis returns the exported genesis state as raw bytes for the module.
 func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
-	gs := am.keeper.ExportGenesis(ctx)
+	gs := am.keeper.ExportGenesis(sdk.UnwrapSDKContext(ctx))
 	return cdc.MustMarshalJSON(gs)
 }
 
@@ -153,26 +156,21 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 
 var _ module.HasConsensusVersion = (*AppModule)(nil)
 
-// ConsensusVersion implements AppModule/ConsensusVersion.
 func (AppModule) ConsensusVersion() uint64 { return 1 }
 
 // ____________________________________________________________________________
 
 var _ appmodule.AppModule = (*AppModule)(nil)
 
-// IsOnePerModuleType implements the depinject.OnePerModuleType interface.
-func (am AppModule) IsOnePerModuleType() {}
+func (AppModule) IsOnePerModuleType() {}
+func (AppModule) IsAppModule() {}
 
-// IsAppModule implements the appmodule.AppModule interface.
-func (am AppModule) IsAppModule() {}
-
-//
+// ----------------------------------------------------------------------------
 // App Wiring Setup
-//
+// ----------------------------------------------------------------------------
 
 func init() {
-	appmodule.Register(
-		&modulev1alpha1.Module{},
+	appmodule.Register(&modulev1alpha1.Module{},
 		appmodule.Provide(ProvideModule),
 	)
 }
@@ -180,9 +178,9 @@ func init() {
 type InterNFTInputs struct {
 	depinject.In
 
-	Config *modulev1alpha1.Module
-	Key    *storetypes.KVStoreKey
+	StoreService store.KVStoreService
 	Cdc    codec.Codec
+	Config *modulev1alpha1.Module
 }
 
 type InterNFTOutputs struct {
@@ -199,7 +197,7 @@ func ProvideModule(in InterNFTInputs) InterNFTOutputs {
 		authority = authtypes.NewModuleAddressOrBech32Address(in.Config.Authority)
 	}
 
-	k := keeper.NewKeeper(in.Key, in.Cdc, authority.String())
+	k := keeper.NewKeeper(in.Cdc, in.StoreService, authority.String())
 	m := NewAppModule(in.Cdc, k)
 
 	return InterNFTOutputs{Keeper: k, Module: m}
