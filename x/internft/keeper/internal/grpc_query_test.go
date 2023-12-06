6038d147e0dd78767f1a1d2decfa653c31aeaf1e
Youngtaek Yoon <noreply@yoon.mailer.me>
2023-12-06 13:57:11 +0000

    Update query server tests

--- x/internft/keeper/internal/grpc_query_test.go
+++ x/internft/keeper/internal/grpc_query_test.go
@@ -1,391 +1,478 @@
 package internal_test
 
 import (
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
-
 	internftv1alpha1 "github.com/0tech/andromeda/x/internft/andromeda/internft/v1alpha1"
 )
 
 func (s *KeeperTestSuite) TestQueryParams() {
-	testCases := map[string]struct {
-		code codes.Code
-	}{
-		"valid request": {},
+	tester := func(subject internftv1alpha1.QueryParamsRequest) error {
+		res, err := s.queryServer.Params(s.ctx, &subject)
+		if err != nil {
+			return err
+		}
+		s.Require().NotNil(res)
+
+		s.Require().NotNil(res.Params)
+
+		return nil
+	}
+	cases := []map[string]Case[internftv1alpha1.QueryParamsRequest]{
+		{
+			"valid request": {
+			},
+		},
 	}
 
-	for name, tc := range testCases {
-		s.Run(name, func() {
-			ctx, _ := s.ctx.CacheContext()
-
-			req := &internftv1alpha1.QueryParamsRequest{}
-
-			res, err := s.queryServer.Params(ctx, req)
-			s.Require().Equal(tc.code, status.Code(err))
-			if tc.code != codes.OK {
-				return
-			}
-			s.Require().NotNil(res)
-
-			// params := res.Params
-		})
-	}
+	doTest(s, tester, cases)
 }
 
 func (s *KeeperTestSuite) TestQueryClass() {
-	testCases := map[string]struct {
-		classID string
-		code    codes.Code
-	}{
-		"valid request": {
-			classID: s.classID,
-		},
-		"invalid class id": {
-			classID: "not/in/caip19",
-			code: codes.InvalidArgument,
-		},
-		"class not found": {
-			classID: "no-class",
-			code:    codes.NotFound,
+	tester := func(subject internftv1alpha1.QueryClassRequest) error {
+		res, err := s.queryServer.Class(s.ctx, &subject)
+		if err != nil {
+			return err
+		}
+		s.Require().NotNil(res)
+
+		s.Require().NotNil(res.Class)
+		s.Require().Equal(subject.ClassId, res.Class.Id)
+
+		return nil
+	}
+	cases := []map[string]Case[internftv1alpha1.QueryClassRequest]{
+		{
+			"nil class id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid class id": {
+				malleate: func(subject *internftv1alpha1.QueryClassRequest) {
+					subject.ClassId = s.classID
+				},
+			},
+			"invalid class id": {
+				malleate: func(subject *internftv1alpha1.QueryClassRequest) {
+					subject.ClassId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
 	}
 
-	for name, tc := range testCases {
-		s.Run(name, func() {
-			ctx, _ := s.ctx.CacheContext()
-
-			req := &internftv1alpha1.QueryClassRequest{
-				ClassId: tc.classID,
-			}
-
-			res, err := s.queryServer.Class(ctx, req)
-			s.Require().Equal(tc.code, status.Code(err))
-			if tc.code != codes.OK {
-				return
-			}
-			s.Require().NotNil(res)
-
-			class := res.Class
-			s.Require().NotNil(class)
-			s.Require().Equal(tc.classID, class.Id)
-		})
-	}
+	doTest(s, tester, cases)
 }
 
 func (s *KeeperTestSuite) TestQueryClasses() {
-	testCases := map[string]struct {
-		code codes.Code
-	}{
-		"valid request": {},
+	tester := func(subject internftv1alpha1.QueryClassesRequest) error {
+		res, err := s.queryServer.Classes(s.ctx, &subject)
+		if err != nil {
+			return err
+		}
+		s.Require().NotNil(res)
+
+		s.Require().Len(res.Classes, 1)
+
+		return nil
+	}
+	cases := []map[string]Case[internftv1alpha1.QueryClassesRequest]{
+		{
+			"valid request": {
+			},
+		},
 	}
 
-	for name, tc := range testCases {
-		s.Run(name, func() {
-			ctx, _ := s.ctx.CacheContext()
-
-			req := &internftv1alpha1.QueryClassesRequest{}
-
-			res, err := s.queryServer.Classes(ctx, req)
-			s.Require().Equal(tc.code, status.Code(err))
-			if tc.code != codes.OK {
-				return
-			}
-			s.Require().NotNil(res)
-
-			classes := res.Classes
-			s.Require().Len(classes, 1)
-		})
-	}
+	doTest(s, tester, cases)
 }
 
 func (s *KeeperTestSuite) TestQueryTrait() {
-	testCases := map[string]struct {
-		classID string
-		traitID string
-		code    codes.Code
-	}{
-		"valid request": {
-			classID: s.classID,
-			traitID: s.immutableTraitID,
+	tester := func(subject internftv1alpha1.QueryTraitRequest) error {
+		res, err := s.queryServer.Trait(s.ctx, &subject)
+		if err != nil {
+			return err
+		}
+		s.Require().NotNil(res)
+
+		s.Require().NotNil(res.Trait)
+		s.Require().Equal(subject.TraitId, res.Trait.Id)
+
+		return nil
+	}
+	cases := []map[string]Case[internftv1alpha1.QueryTraitRequest]{
+		{
+			"nil class id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid class id": {
+				malleate: func(subject *internftv1alpha1.QueryTraitRequest) {
+					subject.ClassId = s.classID
+				},
+			},
+			"invalid class id": {
+				malleate: func(subject *internftv1alpha1.QueryTraitRequest) {
+					subject.ClassId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
-		"invalid class id": {
-			classID: "not/in/caip19",
-			traitID: s.immutableTraitID,
-			code:    codes.InvalidArgument,
-		},
-		"invalid trait id": {
-			classID: s.classID,
-			traitID: "not/in/caip19",
-			code:    codes.InvalidArgument,
-		},
-		"trait not found": {
-			classID: "no-class",
-			traitID: s.immutableTraitID,
-			code:    codes.NotFound,
+		{
+			"nil trait id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid trait id": {
+				malleate: func(subject *internftv1alpha1.QueryTraitRequest) {
+					subject.TraitId = s.immutableTraitID
+				},
+			},
+			"invalid trait id": {
+				malleate: func(subject *internftv1alpha1.QueryTraitRequest) {
+					subject.TraitId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
 	}
 
-	for name, tc := range testCases {
-		s.Run(name, func() {
-			ctx, _ := s.ctx.CacheContext()
-
-			req := &internftv1alpha1.QueryTraitRequest{
-				ClassId: tc.classID,
-				TraitId: tc.traitID,
-			}
-
-			res, err := s.queryServer.Trait(ctx, req)
-			s.Require().Equal(tc.code, status.Code(err))
-			if tc.code != codes.OK {
-				return
-			}
-			s.Require().NotNil(res)
-
-			trait := res.Trait
-			s.Require().NotNil(trait)
-			s.Require().Equal(tc.traitID, trait.Id)
-		})
-	}
+	doTest(s, tester, cases)
 }
 
 func (s *KeeperTestSuite) TestQueryTraits() {
-	testCases := map[string]struct {
-		classID string
-		code    codes.Code
-	}{
-		"valid request": {
-			classID: s.classID,
-		},
-		"invalid class id": {
-			classID: "not/in/caip19",
-			code: codes.InvalidArgument,
+	tester := func(subject internftv1alpha1.QueryTraitsRequest) error {
+		res, err := s.queryServer.Traits(s.ctx, &subject)
+		if err != nil {
+			return err
+		}
+		s.Require().NotNil(res)
+
+		s.Require().Len(res.Traits, 2)
+
+		return nil
+	}
+	cases := []map[string]Case[internftv1alpha1.QueryTraitsRequest]{
+		{
+			"nil class id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid class id": {
+				malleate: func(subject *internftv1alpha1.QueryTraitsRequest) {
+					subject.ClassId = s.classID
+				},
+			},
+			"invalid class id": {
+				malleate: func(subject *internftv1alpha1.QueryTraitsRequest) {
+					subject.ClassId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
 	}
 
-	for name, tc := range testCases {
-		s.Run(name, func() {
-			ctx, _ := s.ctx.CacheContext()
-
-			req := &internftv1alpha1.QueryTraitsRequest{
-				ClassId: tc.classID,
-			}
-
-			res, err := s.queryServer.Traits(ctx, req)
-			s.Require().Equal(tc.code, status.Code(err))
-			if tc.code != codes.OK {
-				return
-			}
-			s.Require().NotNil(res)
-
-			traits := res.Traits
-			s.Require().Len(traits, 2)
-		})
-	}
+	doTest(s, tester, cases)
 }
 
 func (s *KeeperTestSuite) TestQueryToken() {
-	testCases := map[string]struct {
-		classID string
-		code    codes.Code
-	}{
-		"valid request": {
-			classID: s.classID,
+	tester := func(subject internftv1alpha1.QueryTokenRequest) error {
+		res, err := s.queryServer.Token(s.ctx, &subject)
+		if err != nil {
+			return err
+		}
+		s.Require().NotNil(res)
+
+		s.Require().NotNil(res.Token)
+		s.Require().Equal(subject.ClassId, res.Token.ClassId)
+		s.Require().Equal(subject.TokenId, res.Token.Id)
+
+		return nil
+	}
+	cases := []map[string]Case[internftv1alpha1.QueryTokenRequest]{
+		{
+			"nil class id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid class id": {
+				malleate: func(subject *internftv1alpha1.QueryTokenRequest) {
+					subject.ClassId = s.classID
+				},
+			},
+			"invalid class id": {
+				malleate: func(subject *internftv1alpha1.QueryTokenRequest) {
+					subject.ClassId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
-		"invalid class id": {
-			classID: "not/in/caip19",
-			code: codes.InvalidArgument,
-		},
-		"token not found": {
-			classID: "no-class",
-			code:    codes.NotFound,
+		{
+			"nil token id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid token id": {
+				malleate: func(subject *internftv1alpha1.QueryTokenRequest) {
+					subject.TokenId = s.tokenIDs[s.customer.String()]
+				},
+			},
+			"invalid token id": {
+				malleate: func(subject *internftv1alpha1.QueryTokenRequest) {
+					subject.TokenId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
 	}
 
-	for name, tc := range testCases {
-		s.Run(name, func() {
-			ctx, _ := s.ctx.CacheContext()
-
-			req := &internftv1alpha1.QueryTokenRequest{
-				ClassId: tc.classID,
-				TokenId:      s.tokenIDs[s.customer.String()],
-			}
-
-			res, err := s.queryServer.Token(ctx, req)
-			s.Require().Equal(tc.code, status.Code(err))
-			if tc.code != codes.OK {
-				return
-			}
-			s.Require().NotNil(res)
-
-			token := res.Token
-			s.Require().NotNil(token)
-			s.Require().Equal(req.ClassId, token.ClassId)
-			s.Require().Equal(req.TokenId, token.Id)
-		})
-	}
+	doTest(s, tester, cases)
 }
 
 func (s *KeeperTestSuite) TestQueryTokens() {
-	testCases := map[string]struct {
-		classID string
-		code    codes.Code
-	}{
-		"valid request": {
-			classID: s.classID,
-		},
-		"invalid class id": {
-			classID: "not/in/caip19",
-			code: codes.InvalidArgument,
+	tester := func(subject internftv1alpha1.QueryTokensRequest) error {
+		res, err := s.queryServer.Tokens(s.ctx, &subject)
+		if err != nil {
+			return err
+		}
+		s.Require().NotNil(res)
+
+		s.Require().Len(res.Tokens, len(s.tokenIDs) - 1)
+
+		return nil
+	}
+	cases := []map[string]Case[internftv1alpha1.QueryTokensRequest]{
+		{
+			"nil class id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid class id": {
+				malleate: func(subject *internftv1alpha1.QueryTokensRequest) {
+					subject.ClassId = s.classID
+				},
+			},
+			"invalid class id": {
+				malleate: func(subject *internftv1alpha1.QueryTokensRequest) {
+					subject.ClassId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
 	}
 
-	for name, tc := range testCases {
-		s.Run(name, func() {
-			ctx, _ := s.ctx.CacheContext()
-
-			req := &internftv1alpha1.QueryTokensRequest{
-				ClassId: tc.classID,
-			}
-
-			res, err := s.queryServer.Tokens(ctx, req)
-			s.Require().Equal(tc.code, status.Code(err))
-			if tc.code != codes.OK {
-				return
-			}
-			s.Require().NotNil(res)
-
-			tokens := res.Tokens
-			s.Require().Len(tokens, len(s.tokenIDs)-1)
-		})
-	}
+	doTest(s, tester, cases)
 }
 
 func (s *KeeperTestSuite) TestQueryProperty() {
-	testCases := map[string]struct {
-		classID    string
-		traitID string
-		code       codes.Code
-	}{
-		"valid request": {
-			classID:    s.classID,
-			traitID: s.immutableTraitID,
+	tester := func(subject internftv1alpha1.QueryPropertyRequest) error {
+		res, err := s.queryServer.Property(s.ctx, &subject)
+		if err != nil {
+			return err
+		}
+		s.Require().NotNil(res)
+
+		s.Require().NotNil(res.Property)
+		s.Require().Equal(subject.TraitId, res.Property.TraitId)
+
+		return nil
+	}
+	cases := []map[string]Case[internftv1alpha1.QueryPropertyRequest]{
+		{
+			"nil class id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid class id": {
+				malleate: func(subject *internftv1alpha1.QueryPropertyRequest) {
+					subject.ClassId = s.classID
+				},
+			},
+			"invalid class id": {
+				malleate: func(subject *internftv1alpha1.QueryPropertyRequest) {
+					subject.ClassId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
-		"invalid class id": {
-			classID: "not/in/caip19",
-			traitID: s.immutableTraitID,
-			code:       codes.InvalidArgument,
+		{
+			"nil token id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid token id": {
+				malleate: func(subject *internftv1alpha1.QueryPropertyRequest) {
+					subject.TokenId = s.tokenIDs[s.customer.String()]
+				},
+			},
+			"invalid token id": {
+				malleate: func(subject *internftv1alpha1.QueryPropertyRequest) {
+					subject.TokenId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
-		"invalid trait id": {
-			classID: s.classID,
-			traitID: "not/in/caip19",
-			code:    codes.InvalidArgument,
-		},
-		"trait not found": {
-			classID:    "no-class",
-			traitID: s.immutableTraitID,
-			code:       codes.NotFound,
+		{
+			"nil trait id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid trait id": {
+				malleate: func(subject *internftv1alpha1.QueryPropertyRequest) {
+					subject.TraitId = s.immutableTraitID
+				},
+			},
+			"invalid trait id": {
+				malleate: func(subject *internftv1alpha1.QueryPropertyRequest) {
+					subject.TraitId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
 	}
 
-	for name, tc := range testCases {
-		s.Run(name, func() {
-			ctx, _ := s.ctx.CacheContext()
-
-			req := &internftv1alpha1.QueryPropertyRequest{
-				ClassId:    tc.classID,
-				TokenId:      s.tokenIDs[s.customer.String()],
-				TraitId: tc.traitID,
-			}
-
-			res, err := s.queryServer.Property(ctx, req)
-			s.Require().Equal(tc.code, status.Code(err))
-			if tc.code != codes.OK {
-				return
-			}
-			s.Require().NotNil(res)
-
-			property := res.Property
-			s.Require().NotNil(property)
-			s.Require().Equal(tc.traitID, property.TraitId)
-		})
-	}
+	doTest(s, tester, cases)
 }
 
 func (s *KeeperTestSuite) TestQueryProperties() {
-	testCases := map[string]struct {
-		classID string
-		code    codes.Code
-	}{
-		"valid request": {
-			classID: s.classID,
+	tester := func(subject internftv1alpha1.QueryPropertiesRequest) error {
+		res, err := s.queryServer.Properties(s.ctx, &subject)
+		if err != nil {
+			return err
+		}
+		s.Require().NotNil(res)
+
+		s.Require().Len(res.Properties, 2)
+
+		return nil
+	}
+	cases := []map[string]Case[internftv1alpha1.QueryPropertiesRequest]{
+		{
+			"nil class id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid class id": {
+				malleate: func(subject *internftv1alpha1.QueryPropertiesRequest) {
+					subject.ClassId = s.classID
+				},
+			},
+			"invalid class id": {
+				malleate: func(subject *internftv1alpha1.QueryPropertiesRequest) {
+					subject.ClassId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
-		"invalid class id": {
-			classID: "not/in/caip19",
-			code: codes.InvalidArgument,
+		{
+			"nil token id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid token id": {
+				malleate: func(subject *internftv1alpha1.QueryPropertiesRequest) {
+					subject.TokenId = s.tokenIDs[s.customer.String()]
+				},
+			},
+			"invalid token id": {
+				malleate: func(subject *internftv1alpha1.QueryPropertiesRequest) {
+					subject.TokenId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
 	}
 
-	for name, tc := range testCases {
-		s.Run(name, func() {
-			ctx, _ := s.ctx.CacheContext()
-
-			req := &internftv1alpha1.QueryPropertiesRequest{
-				ClassId: tc.classID,
-				TokenId:   s.tokenIDs[s.customer.String()],
-			}
-
-			res, err := s.queryServer.Properties(ctx, req)
-			s.Require().Equal(tc.code, status.Code(err))
-			if tc.code != codes.OK {
-				return
-			}
-			s.Require().NotNil(res)
-
-			properties := res.Properties
-			s.Require().Len(properties, 2)
-		})
-	}
+	doTest(s, tester, cases)
 }
 
 func (s *KeeperTestSuite) TestQueryOwner() {
-	testCases := map[string]struct {
-		classID string
-		code    codes.Code
-	}{
-		"valid request": {
-			classID: s.classID,
+	tester := func(subject internftv1alpha1.QueryOwnerRequest) error {
+		res, err := s.queryServer.Owner(s.ctx, &subject)
+		if err != nil {
+			return err
+		}
+		s.Require().NotNil(res)
+
+		s.Require().NotEmpty(res.Owner)
+
+		return nil
+	}
+	cases := []map[string]Case[internftv1alpha1.QueryOwnerRequest]{
+		{
+			"nil class id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid class id": {
+				malleate: func(subject *internftv1alpha1.QueryOwnerRequest) {
+					subject.ClassId = s.classID
+				},
+			},
+			"invalid class id": {
+				malleate: func(subject *internftv1alpha1.QueryOwnerRequest) {
+					subject.ClassId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
-		"invalid class id": {
-			classID: "not/in/caip19",
-			code: codes.InvalidArgument,
-		},
-		"token not found": {
-			classID: "no-class",
-			code:    codes.NotFound,
+		{
+			"nil token id": {
+				err: func() error {
+					return internftv1alpha1.ErrUnimplemented
+				},
+			},
+			"valid token id": {
+				malleate: func(subject *internftv1alpha1.QueryOwnerRequest) {
+					subject.TokenId = s.tokenIDs[s.customer.String()]
+				},
+			},
+			"invalid token id": {
+				malleate: func(subject *internftv1alpha1.QueryOwnerRequest) {
+					subject.TokenId = "not/in/caip19"
+				},
+				err: func() error {
+					return internftv1alpha1.ErrInvalidID
+				},
+			},
 		},
 	}
 
-	for name, tc := range testCases {
-		s.Run(name, func() {
-			ctx, _ := s.ctx.CacheContext()
-
-			req := &internftv1alpha1.QueryOwnerRequest{
-				ClassId: tc.classID,
-				TokenId:   s.tokenIDs[s.customer.String()],
-			}
-
-			res, err := s.queryServer.Owner(ctx, req)
-			s.Assert().Equal(tc.code, status.Code(err), err)
-			if tc.code != codes.OK {
-				return
-			}
-			s.Require().NotNil(res)
-
-			ownerStr := res.Owner
-			_, err = sdk.AccAddressFromBech32(ownerStr)
-			s.Require().NoError(err)
-		})
-	}
+	doTest(s, tester, cases)
 }
